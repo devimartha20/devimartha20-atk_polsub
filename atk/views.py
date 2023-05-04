0613200799df93501531a1ecefe9744295ea241b
devimartha20 <devimarthareal@gmail.com>
2023-05-04 12:33:10 +0700

    url

--- atk/views.py
+++ atk/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+# AUTHENTICATION
+def login(request):
+    return render(request, 'atk/login.html')
+
+def resetPassword(request):
+    return HttpResponse('reset password')
+
+def home(request):
+    return render(request, 'atk/home.html')
